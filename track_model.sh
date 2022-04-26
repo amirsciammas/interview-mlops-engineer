ac09620011c60e5f4545d390bb5ba2553b28492b
avillalbacantero <villalbacanteroa@gmail.com>
2022-04-26 20:54:53 +0200

    [Model Tracking - Pipeline]: Add model tracking in the pipeline.

---
+++ track_model.sh
@@ -0,0 +1,13 @@
+# Steps to track and push a model to a S3 bucket
+
+cp models/my_best_model.h5 tracked_models/my_best_model.h5
+echo "Send model to tracking directory"
+
+dvc add tracked_models/my_best_model.h5
+git add tracked_models/my_best_model.h5.dvc .gitignore
+git commit -m "Trained model tracked with DVC"
+echo "Tracking trained model!"
+
+# Now that the model is tracked and versioned, push the current version to S3
+dvc push tracked_models/my_best_model.h5.dvc
+echo "Trained model versioned and saved on the configured S3 bucket"
